65e92efda2360a41e7206a9cc18e8e7e93e0b4eb
= <l.natus@live.com>
2017-11-16 16:15:46 +0100

    Add thankyou.html skeleton

--- src/scripts/app/ts/app.ts
+++ src/scripts/app/ts/app.ts
@@ -5,24 +5,6 @@ namespace DA {
     private emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     public run () {
-
-      $("#da-contact-form").submit(function(event) {
-        event.preventDefault();
-        const $form = $( this ), url = $form.attr('action');
-        const post = $.post(
-          url,
-          {
-            name: $('#inputName').val(),
-            email: $('#inputEmail').val(),
-            phone: $('#inputPhone').val(),
-            request: $('#inputRequest').val()
-        });
-
-        post.done((data) => {
-          console.log(data);
-        });
-      });
-
       const $name = $('#inputName')
       const $email = $('#inputEmail')
       const $request = $('#inputRequest')
