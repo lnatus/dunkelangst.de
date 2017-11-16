326c9294557d544050d1c74220d99e44d039c420
= <l.natus@live.com>
2017-11-16 16:05:36 +0100

    Try to submit via ajax

--- src/scripts/app/ts/app.ts
+++ src/scripts/app/ts/app.ts
@@ -5,6 +5,24 @@ namespace DA {
     private emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     public run () {
+
+      $("#da-contact-form").submit(function(event) {
+        event.preventDefault();
+        const $form = $( this ), url = $form.attr('action');
+        const post = $.post(
+          url,
+          {
+            name: $('#inputName').val(),
+            email: $('#inputEmail').val(),
+            phone: $('#inputPhone').val(),
+            request: $('#inputRequest').val()
+        });
+
+        post.done((data) => {
+          alert(data);
+        });
+      });
+
       const $name = $('#inputName')
       const $email = $('#inputEmail')
       const $request = $('#inputRequest')
